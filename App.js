8ff214b0d658eb27be5b72e3f1d5600f39ee42af
codu2 <khub0929@gmail.com>
2022-12-14 00:21:07 +0900

    native-stack, bottom-tabs navigator를 이용한 내비게이션 설정 및 screenOptions 추가, expo-font를 이용한 custom fonts loading 및 expo-app-loading을 이용한 splash screen 연장

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
-export default function App() {
+import Category from "./screens/Category";
+import Search from "./screens/Search";
+import Map from "./screens/Map";
+import MyTicket from "./screens/MyTicket";
+import Login from "./screens/Login";
+
+import { Colors } from "./constants/styles";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function BottomTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      sceneContainerStyle={{ backgroundColor: Colors.primary700 }}
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: Colors.primary700,
+          marginBottom: 16,
+          marginHorizontal: 16,
+          borderRadius: 24,
+          height: 64,
+          paddingBottom: 12,
+          paddingTop: 8,
+          paddingHorizontal: 8,
+        },
+        tabBarLabelStyle: { marginTop: 4, fontFamily: "line-bold" },
+        tabBarActiveTintColor: Colors.accent700,
+        tabBarIcon: ({ focused }) => {
+          const color = focused ? Colors.accent700 : Colors.primary600;
+
+          switch (route.name) {
+            case "Category":
+              return <Ionicons name="home-outline" color={color} size={24} />;
+            case "Search":
+              return <Ionicons name="search-sharp" color={color} size={24} />;
+            case "Map":
+              return <Ionicons name="map-outline" color={color} size={24} />;
+            case "MyTicket":
+              return (
+                <MaterialCommunityIcons
+                  name="ticket-outline"
+                  color={color}
+                  size={24}
+                />
+              );
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Category" component={Category} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="MyTicket" component={MyTicket} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "line-thin": require("./assets/fonts/LINESeedSans_A_Th.ttf"),
+    "line-regular": require("./assets/fonts/LINESeedSans_A_Rg.ttf"),
+    "line-bold": require("./assets/fonts/LINESeedSans_A_Bd.ttf"),
+    "line-extraBold": require("./assets/fonts/LINESeedSans_A_XBd.ttf"),
+    "line-heavy": require("./assets/fonts/LINESeedSans_A_He.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="BottomTabs"
+            component={BottomTabs}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
