73dc62dd56b6845b0f6b488bf782b1eec0f41ef9
codu2 <khub0929@gmail.com>
2022-12-17 23:48:54 +0900

    선택한 교통수단과 출발지에서 목적지까지의 거리, 이동시간, 출발지와 목적지의 주소를 저장하는 로직 추가

--- store/coordinate.js
+++ store/coordinate.js
@@ -1,23 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  transport: null,
   origin: null,
   destination: null,
+  distance: null,
+  duration: null,
+  address: {
+    start: null,
+    end: null,
+  },
 };
 
 const coordinateSlice = createSlice({
   name: "coordinate",
   initialState,
   reducers: {
+    selectTransport: (state, action) => {
+      state.transport = action.payload;
+    },
     getOrigin: (state, action) => {
       state.origin = action.payload;
     },
     getDestination: (state, action) => {
       state.destination = action.payload;
     },
+    getDistanceAndDuration: (state, action) => {
+      state.distance = action.payload.distance;
+      state.duration = action.payload.duration;
+    },
+    getAddress: (state, action) => {
+      state.address = { start: action.payload.start, end: action.payload.end };
+    },
   },
 });
 
-export const { getOrigin, getDestination } = coordinateSlice.actions;
+export const {
+  selectTransport,
+  getOrigin,
+  getDestination,
+  getDistanceAndDuration,
+  getAddress,
+} = coordinateSlice.actions;
 
 export default coordinateSlice.reducer;
